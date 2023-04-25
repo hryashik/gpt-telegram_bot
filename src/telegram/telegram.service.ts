58eb903e07f44541dd40f19f8992165a04008919
Dmitriy <tsukikohaya@gmail.com>
2023-04-25 15:04:35 +0300

    update: telegram onMessage

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Start, Update, Ctx, Message, On } from 'nestjs-telegraf';
+import { ChatgptService } from 'src/chatgpt/chatgpt.service';
+import { Scenes, Telegraf } from 'telegraf';
 
-@Injectable()
-export class TelegramService {}
+type Context = Scenes.SceneContext;
+
+@Update()
+export class TelegramService extends Telegraf<Context> {
+    @Start()
+    onStart(@Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<b>Привет, ${ctx.from.username}</b>
+Это чат бот с ChatGPT!
+Введите любую фразу и получите ответ!
+        `);
+    }
+    @On('text')
+    onMessage(@Message('text') msg: string, @Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<i>${msg}</i>`);
+    }
+}
