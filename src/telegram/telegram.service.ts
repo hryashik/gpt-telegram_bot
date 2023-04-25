170eb36e655c42cd5522f5c2aff9b92a9f0d89fe
Dmitriy <tsukikohaya@gmail.com>
2023-04-25 15:33:02 +0300

    fixed errors

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,12 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { Start, Update, Ctx, Message, On } from 'nestjs-telegraf';
-import { ChatgptService } from 'src/chatgpt/chatgpt.service';
+import { ChatgptService } from '@/chatgpt/chatgpt.service';
 import { Scenes, Telegraf } from 'telegraf';
+import { Logger } from '@nestjs/common';
 
 type Context = Scenes.SceneContext;
 
 @Update()
 export class TelegramService extends Telegraf<Context> {
+    private readonly logger = new Logger(ChatgptService.name);
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly gpt: ChatgptService,
+    ) {
+        super(configService.get('TELEGRAM_API'));
+    }
     @Start()
     onStart(@Ctx() ctx: Context) {
         ctx.replyWithHTML(`<b>Привет, ${ctx.from.username}</b>
@@ -16,6 +24,7 @@ export class TelegramService extends Telegraf<Context> {
     }
     @On('text')
     onMessage(@Message('text') msg: string, @Ctx() ctx: Context) {
-        ctx.replyWithHTML(`<i>${msg}</i>`);
+        this.logger.log(msg);
+        return this.gpt.generateResponse(msg);
     }
 }
